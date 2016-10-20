5c3ad2fe77135af7d085473b9dafc74d61087602
eufracia <ziporalicia@gmail.com>
2016-10-19 19:16:23 -0600

    Add Intro folder

--- client/components/Intro/index.js
+++ client/components/Intro/index.js
@@ -7,10 +7,10 @@ import styles from './styles.css';
 class Intro extends React.Component {
   render() {
     return(
-      <section className={styles.root}>
+      <section className={styles.container}>
         <div>Logo</div>
-        <h1>Eufracia Marsella</h1>
-        <p>Hi! I'm an application and web developer based in Edmonton, Alberta.
+        <h1 className={styles.name}>Eufracia Marsella</h1>
+        <p className={styles.description}>Hi! I'm an application and web developer based in Edmonton, Alberta.
         I'm passionate about learning new programming languages and build some
         applications from it. In my spare time, I love to dance, read some books,
         hangout with friends and try out new food. Let's meet and we can talk more!</p>
