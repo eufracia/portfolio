cc1ba5cb7b16c379ac115d46328dfcf1f516b1ea
eufracia <ziporalicia@gmail.com>
2016-10-30 17:26:34 -0600

    Uodate the app folder

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -8,7 +8,7 @@ export default class App extends Component {
       <Helmet title='Go + React + Redux = rocks!' />
       {this.props.children}
       <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,600" rel="stylesheet" />
     </div>;
   }
 
