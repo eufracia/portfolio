21af10d7d09cc7499ccf0f13adc93cbe50bfc313
eufracia <ziporalicia@gmail.com>
2016-10-30 17:25:30 -0600

    Add and update Timeline folder

---
+++ client/components/Timeline/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import styles from './styles.css';
+
+const Event = (props) => <div className={`${styles.dot} ${styles[`dot${props.num}`]}`}>
+  { props.desc && <div className={styles.line}>
+    <div className={styles.desc}>{props.desc}</div>
+  </div> }
+  { props.year && <div className={styles.year}>{props.year}</div> }
+</div>
+
+class Timeline extends React.Component {
+  render() {
+    return(
+      <section>
+        <div className={styles.container}>
+          <div className={styles.header}>
+           <h1>Timeline</h1>
+           <p>Different journeys in my life constantly developed and growth. </p>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.timeline}>
+           <Event num={1} desc="High School" year="2009-2012" />
+           <Event num={2} desc="NAIT" year="2014-2016" />
+           <Event num={3} desc="C#" />
+           <Event num={4} desc="ASP.NET" />
+           <Event num={5} desc="Javascript" />
+           <Event num={6} desc="HTML5 & CSS3" />
+           <Event num={7} desc="iOS" />
+           <Event num={8} desc="Self Learning" year="2016" />
+           <Event num={9} desc="React" />
+           <Event num={10} desc="Go" />
+         </div>
+        </div>
+      </section>
+    )
+  }
+};
+
+export default Timeline;
