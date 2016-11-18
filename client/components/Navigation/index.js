2d68d582ad61332d2723c01d59e4b0ccf609f8e5
eufracia <ziporalicia@gmail.com>
2016-11-17 21:21:45 -0700

    Update Navigation folder

--- client/components/Navigation/index.js
+++ client/components/Navigation/index.js
@@ -4,14 +4,38 @@ import {render} from 'react-dom';
 import styles from './styles.css';
 
 class Navigation extends React.Component {
-  render() {
-    return (
-      <ul className={styles.navbar}>
-         <li><a href="#timeline">Timeline</a></li>
-         <li><a href="#projects">Projects</a></li>
-      </ul>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      y: 0,
+    };
+    this.onScroll = this.onScroll.bind(this);
   }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll);
+    this.setState({ y: window.pageYOffset || document.documentElement.scrollTop });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  onScroll(e) {
+    this.setState({ y: window.pageYOffset || document.documentElement.scrollTop });
+  }
+
+  render() {
+    const { y } = this.state;
+    return <nav className={`${styles.container} ${y > 200 && styles.offset}`}>
+      <ul className={styles.navbar}>
+        <li><a href="#timeline">Timeline</a></li>
+        <li><a href="#projects">Projects</a></li>
+      </ul>
+    </nav>
+  }
+
 }
 
 export default  Navigation;
