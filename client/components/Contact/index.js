df0a7b6fcfe315be22a9f39c853fcc62f1c646fa
eufracia <ziporalicia@gmail.com>
2016-11-17 21:20:16 -0700

    Add Contact folder

---
+++ client/components/Contact/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSdc8Avbag73gP6NiBr4QokMgBaj85XZmQnoFV5XCKz8EH16Wg/formResponse">
+          <input type="text" />
+          <input type="text" />
+          <textarea />
+        </form>
+      </div>
+     );
+  }
+}
